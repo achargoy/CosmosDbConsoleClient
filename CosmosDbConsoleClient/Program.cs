c5383d23d246b337195b1d84d2487e8330cf096c
Alberto Chargoy <achargoy@hotmail.com>
2022-02-03 14:30:17 -0500

    Creating the console project

---
+++ CosmosDbConsoleClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CosmosDbConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
